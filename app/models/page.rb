41abb0f8917324f57ac258bd4c77584510016f9c
prabhakar <undurthi.prabhakar@gmail.com>
2014-04-28 16:58:04 +0530

    Create models for Pages, Sections, Subjects.

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,9 @@
 class Page < ActiveRecord::Base
+
+	belongs_to :subject
+	has_and_belongs_to_many :editors , :class_name => "AdminUser"
+
+
+	#Key Tip, the foreign key always goes to the
+	# belongs_to 
 end
